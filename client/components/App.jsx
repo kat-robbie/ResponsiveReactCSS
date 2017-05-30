361a9360f3ecec24aedb9d83269d2d81f0840d6a
Kat Robbie <katherine.robbie@gmail.com>
2017-05-30 13:55:49 +1200

    add aside2 to app

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Header from './Header'
 import Main from './Main'
 import Aside1 from './Aside1'
+import Aside2 from './Aside2'
 
 
 function App () {
@@ -11,6 +12,7 @@ function App () {
       <Header />
       <Main />
       <Aside1 />
+      <Aside2 />
     </div>
   )
 }
